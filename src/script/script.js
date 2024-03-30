d0052ca4862bab89d0419f28b5015ed6ccfb0e04
anhkhoakz <113995598+anhkhoakz@users.noreply.github.com>
2024-03-30 16:07:08 +0700

    Add favicon and update toggle button functionality

--- src/script/script.js
+++ src/script/script.js
@@ -1,4 +1,3 @@
-// add class navbarDark on navbar scroll
 const header = document.querySelector(".navbar");
 console.log(header);
 window.onscroll = function () {
@@ -9,7 +8,7 @@ window.onscroll = function () {
         header.classList.remove("navbarDark");
     }
 };
-// collapse navbar after click on small devices
+
 const navLinks = document.querySelectorAll(".nav-item");
 const menuToggle = document.getElementById("navbarSupportedContent");
 
@@ -19,29 +18,20 @@ navLinks.forEach((l) => {
     });
 });
 
-// Handle the beforeunload event
 // window.addEventListener("beforeunload", function (e) {
 //     e.preventDefault();
 //     e.returnValue = "";
 //     return "Are you sure you want to leave this page?";
 // });
 
-// // Toggle button functionality
-// const toggleButton = document.getElementById("toggleButton");
-// const collapseContent = document.getElementById("collapseContent");
+const toggleButton = document.getElementById("toggleButton");
+let isExpanded = false;
 
-// collapseContent.addEventListener("shown.bs.collapse", function () {
-//     toggleButton.textContent = "Show Less";
-// });
-
-// collapseContent.addEventListener("hidden.bs.collapse", function () {
-//     toggleButton.textContent = "Show More";
-// });
-
-// toggleButton.addEventListener("click", function () {
-//     if (collapseContent.classList.contains("show")) {
-//         toggleButton.textContent = "Show More";
-//     } else {
-//         toggleButton.textContent = "Show Less";
-//     }
-// });
+toggleButton.addEventListener("click", function () {
+    if (isExpanded) {
+        toggleButton.textContent = "Show More";
+    } else {
+        toggleButton.textContent = "Show Less";
+    }
+    isExpanded = !isExpanded;
+});
